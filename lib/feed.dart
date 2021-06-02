1753fbb6a01ea43abb53cb9280fd8ed2e3383046
aditya <adicadi158@gmail.com>
2021-06-02 19:55:19 +0530

    Video playback fixed

--- lib/feed.dart
+++ lib/feed.dart
@@ -24,6 +24,8 @@ class _FeedState extends State<Feed> {
     _controller = VideoPlayerController.network(
         'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4');
     _initializeVideoPlayerFuture = _controller.initialize();
+    _controller.setLooping(true);
+    _controller.play();
   }
 
   @override
@@ -115,16 +117,11 @@ class _FeedState extends State<Feed> {
                               builder: (context, snapshot) {
                                 if (snapshot.connectionState ==
                                     ConnectionState.done) {
-                                  // If the VideoPlayerController has finished initialization, use
-                                  // the data it provides to limit the aspect ratio of the video.
                                   return AspectRatio(
                                     aspectRatio: _controller.value.aspectRatio,
-                                    // Use the VideoPlayer widget to display the video.
                                     child: VideoPlayer(_controller),
                                   );
                                 } else {
-                                  // If the VideoPlayerController is still initializing, show a
-                                  // loading spinner.
                                   return Center(
                                       child: CircularProgressIndicator());
                                 }
--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Influencer',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: HexColor('4661df'),
       ),
